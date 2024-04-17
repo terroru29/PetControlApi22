b87fc2339d9c0e76ff6c160af4c80829629330c3
patricit <patriformacion10@gmail.com>
2024-04-17 19:36:06 +0200

    Added activity login

---
+++ app/src/main/java/net/petcontrol/PetControlApi22/LoginPetControl.java
@@ -0,0 +1,14 @@
+package net.petcontrol.PetControlApi22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginPetControl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_petcontrol);
+    }
+}
