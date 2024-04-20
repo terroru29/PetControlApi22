fa693e0b8bf5d83b0b6df20e25b0444b4520bb49
patricit <patriformacion10@gmail.com>
2024-04-20 12:01:42 +0200

    Picture of gallery in Register User

--- app/src/main/java/net/petcontrol/PetControlApi22/RegisterPetControl.java
+++ app/src/main/java/net/petcontrol/PetControlApi22/RegisterPetControl.java
@@ -3,21 +3,26 @@ package net.petcontrol.PetControlApi22;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.provider.OpenableColumns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class RegisterPetControl extends AppCompatActivity {
 
     Button searchPicture;
     TextView pictureSelected;
+    ImageView pictureUser;
     private static final int REQUEST_CODE_FILE_PICKER = 1;
+    private static final int PICK_IMAGE_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,7 @@ public class RegisterPetControl extends AppCompatActivity {
         // Asociar variables con sus recursos
         searchPicture = findViewById(R.id.btnPicture);
         pictureSelected = findViewById(R.id.txtPictureSelected);
+        pictureUser = findViewById(R.id.ivUser);
 
         //**Evento de botón
         searchPicture.setOnClickListener(new View.OnClickListener() {
@@ -59,24 +65,36 @@ public class RegisterPetControl extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == REQUEST_CODE_FILE_PICKER && resultCode == RESULT_OK && data != null) {
+        if ((requestCode == REQUEST_CODE_FILE_PICKER || requestCode == PICK_IMAGE_REQUEST )
+                && resultCode == RESULT_OK && data != null) {
             // Obtenemos la URI del archivo seleccionado
             Uri selectedFileUri = data.getData();
 
             // Obtiene el nombre de la imagen seleccionada
             String namePicture = getFileName(selectedFileUri);
 
-            // Muestra el nombre de la imagen seleccionada debajo de ésta
+            // Obtenemos la imagen seleccionada por el usuario
+            pictureUser.setImageURI(selectedFileUri);
+
+            // Muestra el nombre de la imagen seleccionada
             pictureSelected.setText(namePicture);
         }
     }
 
+    /**
+     * Obtener la imagen de la galería.
+     */
+    private void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
     /**
      * Obtener el nombre del archivo a partir de su URI
      *
      * @param uri Path of the picture
      * @return picName Name of the picture
      */
+    @SuppressLint("Range")
     private String getFileName(Uri uri) {
         String picName = null;
         ContentResolver contentResolver = getContentResolver();
