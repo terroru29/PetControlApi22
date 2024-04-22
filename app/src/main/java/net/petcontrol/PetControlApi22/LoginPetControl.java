0ed18f9e97f5544a1572a6114aa2ee525203cfad
patricit <patriformacion10@gmail.com>
2024-04-22 21:29:37 +0200

    Add validation of e-mail

--- app/src/main/java/net/petcontrol/PetControlApi22/LoginPetControl.java
+++ app/src/main/java/net/petcontrol/PetControlApi22/LoginPetControl.java
@@ -6,18 +6,25 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.Spannable;
 import android.text.SpannableString;
+import android.text.TextWatcher;
 import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginPetControl extends AppCompatActivity {
 
     Button logIn;
-    TextView terms, signUp, validationEmail;
-    String correoElectronico, requirements, conditions;
+    TextView terms, signUp, messageEmail;
+    EditText validationEmail, validationPass;
+    String email, mensajeEmailCorrect, cadenaCorrecta, mensajeEmailIncorrect, cadenaIncorrecta,
+            requirements, conditions;
+    // Permitirá cualquier carácter (sin @), @, cualquier carácter (sin @), ., de 2 a 3 letras minúsculas
+    private static final String EMAIL_PATTERN = "^[^@]+@+[^@]+\\.[a-z]{2,3}+$";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,19 +34,52 @@ public class LoginPetControl extends AppCompatActivity {
         logIn = findViewById(R.id.btnLogIn);
         terms = findViewById(R.id.switchTerms);
         signUp = findViewById(R.id.txtSignUp);
-        validationEmail = findViewById(R.id.txtValidationEmail);
+        validationEmail = findViewById(R.id.etEmail);
+        messageEmail = findViewById(R.id.txtValidationEmail);
+        validationPass = findViewById(R.id.etPassword);
 
-        /*
-        correoElectronico = (String) validationEmail.getText();
-        // Validación de correo --> Si funciona aparece en verde, sino, en rojo
-        if (emailValidation(correoElectronico))
-            // Ponerlo en recurso String
-            validationEmail.setText("El correo tiene el formato correcto.");
-        else {
-            validationEmail.setText("El correo no tiene el formato correcto.");
-            validationEmail.setTextColor(Color.RED);
-        }
-         */
+
+        //-Evento EditText
+        //--Email
+        validationEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                email = validationEmail.getText().toString();
+                // Validación de correo --> Si es correcto aparece en verde, sino, en rojo
+                if (emailValidation(email)) {
+                    mensajeEmailCorrect = getResources().getString(R.string.validation_email);
+                    cadenaCorrecta = String.format(mensajeEmailCorrect);
+                    messageEmail.setText(cadenaCorrecta);
+                    messageEmail.setTextColor(Color.parseColor("#FF4DBC34"));
+                }
+                else {
+                    mensajeEmailIncorrect = getResources().getString(R.string.invalidation_email);
+                    cadenaIncorrecta = String.format(mensajeEmailIncorrect);
+                    messageEmail.setText(cadenaIncorrecta);
+                    messageEmail.setTextColor(Color.RED);
+                }
+            }
+        });
+        //--Password
+        validationPass.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
 
 
         // ----- CAMBIAR COLOR DEL TEXTO DEL SWITCH -----
@@ -88,13 +128,53 @@ public class LoginPetControl extends AppCompatActivity {
     /**
      *  Método que valida el correo electrónico: Mín. 1 @ y 1 . al final con la extensión (3-4 letras)
      *
-     * @param correo Correo electrónico del usuario
-     * @return Variable que indica si el correo está bien o no
+     * @param email Correo electrónico del usuario para validar
+     * @return true si el correo es correcto; false si es incorrecto
      */
-    public boolean emailValidation(String correo) {
-        boolean correcto = false;
+    public boolean emailValidation(String email) {
+        boolean correct = false;
+        int countArroba = 0, countPoint = 0;
 
-        //if (correo.contains())
-        return correcto;
+        if (email != null) {
+            for (int i = 0; i < email.length(); i++) {
+                if (email.charAt(i) == '@') {
+                    countArroba++;
+                }
+                if (email.charAt(i) == '.') {
+                    countPoint++;
+                }
+            }
+            if (countArroba == 1 && countPoint >= 1 && email.matches(EMAIL_PATTERN))
+                correct = true;
+        }
+        return correct;
+    }
+
+    /**
+     *
+     * @param pass Contraseña del usuario a validar --> Mínimo una minúscula, una mayúscula,
+     *             un dígito, un carácter especial y que tenga una longitud entre 8 y 12 caracteres.
+     * @return true si cumple los requisitos anteriores; false en caso contrario
+     */
+    public boolean passValidation(String pass) {
+        boolean correct = false;
+        int countLow = 0, countUp = 0, countNum = 0, countSpe = 0;
+
+        if (pass != null) {
+            for (int i = 0; i < pass.length(); i++) {
+                if (pass.charAt(i) >= 'a' && pass.charAt(i) <= 'z')
+                    countLow++;
+                else if (pass.charAt(i) >= 'A' && pass.charAt(i) <= 'Z')
+                    countUp++;
+                else if (pass.charAt(i) >= '0' && pass.charAt(i) <= '9')
+                    countNum++;
+                else
+                    countSpe++;
+            }
+            if (countLow >= 1 && countUp >= 1 && countNum >= 1 &&
+                    (pass.length() >= 8 && pass.length() <= 12) && countSpe >= 1)
+                correct = true;
+        }
+        return correct;
     }
 }
