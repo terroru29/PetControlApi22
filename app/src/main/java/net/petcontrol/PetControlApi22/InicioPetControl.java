c0fc008b1a9c192cb251bb90d86ac2662c52d694
patricit <patriformacion10@gmail.com>
2024-04-27 13:25:57 +0200

    PORTADA

--- app/src/main/java/net/petcontrol/PetControlApi22/InicioPetControl.java
+++ app/src/main/java/net/petcontrol/PetControlApi22/InicioPetControl.java
@@ -20,8 +20,8 @@ public class InicioPetControl extends AppCompatActivity {
 
     FloatingActionButton fab;
     Animation aparecer, moving;
-    ImageView ivLogo;
-    TextView txtEslogan;
+    ImageView logoPC;
+    TextView eslogan;
 
     // PRUEBA
     Animation aparecer1, aparecer2, aparecer3, aparecer4, aparecer5, aparecer6, aparecer7,
@@ -45,46 +45,40 @@ public class InicioPetControl extends AppCompatActivity {
         R1 = findViewById(R.id.tituloR);
         O2 = findViewById(R.id.tituloO2);
         L = findViewById(R.id.tituloL);
-        ivLogo = findViewById(R.id.logoPC);
-        txtEslogan = findViewById(R.id.eslogan);
+        logoPC = findViewById(R.id.ivLogo);
+        eslogan = findViewById(R.id.txtEslogan);
         fab = findViewById(R.id.floatingActionButton);
 
 
         // ANIMACIONES
-        metodoAnimacion(aparecer1, R.anim.appearing1, P);
-        metodoAnimacion(aparecer2, R.anim.appearing2, E);
-        metodoAnimacion(aparecer3, R.anim.appearing3, T);
-        metodoAnimacion(aparecer4, R.anim.appearing4, C);
-        metodoAnimacion(aparecer5, R.anim.appearing5, O);
-        metodoAnimacion(aparecer6, R.anim.appearing6, N);
-        metodoAnimacion(aparecer7, R.anim.appearing7, T2);
-        metodoAnimacion(aparecer8, R.anim.appearing8, R1);
-        metodoAnimacion(aparecer9, R.anim.appearing9, O2);
-        metodoAnimacion(aparecer10, R.anim.appearing10, L);
-        metodoAnimacion(aparecer, R.anim.appearing, ivLogo);
+        showAnimation(aparecer1, R.anim.appearing1, P);
+        showAnimation(aparecer2, R.anim.appearing2, E);
+        showAnimation(aparecer3, R.anim.appearing3, T);
+        showAnimation(aparecer4, R.anim.appearing4, C);
+        showAnimation(aparecer5, R.anim.appearing5, O);
+        showAnimation(aparecer6, R.anim.appearing6, N);
+        showAnimation(aparecer7, R.anim.appearing7, T2);
+        showAnimation(aparecer8, R.anim.appearing8, R1);
+        showAnimation(aparecer9, R.anim.appearing9, O2);
+        showAnimation(aparecer10, R.anim.appearing10, L);
+        showAnimation(aparecer, R.anim.appearing, logoPC);
 
-        // Hacer que el texto esté oculto inicialmente
-        txtEslogan.setVisibility(View.INVISIBLE);
+        // Ocultar el eslogan al principio
+        //eslogan.setVisibility(View.INVISIBLE);
 
-        /*
-        // Usar un Handler para retrasar la visibilidad y luego aplicar una animación para aparecer suavemente
+        // Usar un Handler para retrasar la visibilidad del eslogan
         new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            /*
             // Crear una animación de desvanecimiento para que el texto aparezca suavemente
             Animation fadeIn = new AlphaAnimation(0, 1);
-            fadeIn.setDuration(2500); // Duración de la animación (1 segundo)
-
-            txtEslogan.startAnimation(fadeIn);
-            txtEslogan.setVisibility(View.VISIBLE);
+            // Duración de la animación (2.5 segundos)
+            fadeIn.setDuration(2500);
+            eslogan.startAnimation(fadeIn);
+            */
+            eslogan.setVisibility(View.VISIBLE);
+            showAnimation(moving, R.anim.traslado, eslogan);
         }, 5000); // 5000 ms de retraso (5 segundos)
-        */
-        // Usar un Handler para hacer que el texto sea visible después de 5 segundos (5000 ms)
-        new Handler(Looper.getMainLooper()).postDelayed(() -> {
-            txtEslogan.setVisibility(View.VISIBLE);
-            metodoAnimacion(moving, R.anim.traslado, txtEslogan);
-        }, 5000); // 5000 ms de retraso (5 segundos)
-
     }
-
     /**
      * Pasará a la ventana indicada --> Iniciar sesión/Registrarse
      *
@@ -96,15 +90,14 @@ public class InicioPetControl extends AppCompatActivity {
         Intent i = new Intent(this, LoginPetControl.class);
         startActivity(i);
     }
-
     /**
      * Proyectará la animación sobre el elemento pasados como argumentos
      *
      * @param animation Objeto que almacenará la animación
      * @param anim Archivo de animación
-     * @param view Elemento sobre el que se va a proyectar
+     * @param view Elemento sobre el que se va a proyectar la animación
      */
-    public void metodoAnimacion(Animation animation, int anim, View view) {
+    public void showAnimation(Animation animation, int anim, View view) {
         // Asignación para que cargue la animación (aparición del logo)
         animation = AnimationUtils.loadAnimation(this, anim);
         // Se resetea la animación
@@ -112,31 +105,4 @@ public class InicioPetControl extends AppCompatActivity {
         // Empezamos la animación sobre la imagen (mediante su variable) que queremos que aparezca
         view.startAnimation(animation);
     }
-    /*
-    public void animacionAparecer() {
-        // Asignación para que cargue la animación (aparición del logo)
-        aparecer = AnimationUtils.loadAnimation(this, R.anim.appearing);
-        // Se resetea la animación
-        aparecer.reset();
-        // Empezamos la animación sobre la imagen (mediante su variable) que queremos que aparezca
-        ivLogo.startAnimation(aparecer);
-    }
-    public void animacionTraslado() {
-        // Asignación para que cargue la animación (aparición del logo)
-        moving = AnimationUtils.loadAnimation(this, R.anim.traslado);
-        // Se resetea la animación
-        moving.reset();
-        // Empezamos la animación sobre la imagen (mediante su variable) que queremos que aparezca
-        txtEslogan.startAnimation(moving);
-    }
-    */
-    /*
-    public void espera() {
-        try {
-            Thread.sleep(10000);c
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    */
 }
