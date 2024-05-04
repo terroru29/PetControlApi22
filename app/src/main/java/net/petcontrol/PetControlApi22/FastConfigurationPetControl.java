8b3d503b917f58b39fc7aa4802d30d4132bac4d1
patricit <patriformacion10@gmail.com>
2024-05-04 14:12:52 +0200

    Fast configuration

--- app/src/main/java/net/petcontrol/PetControlApi22/FastConfigurationPetControl.java
+++ app/src/main/java/net/petcontrol/PetControlApi22/FastConfigurationPetControl.java
@@ -2,13 +2,27 @@ package net.petcontrol.PetControlApi22;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FastConfigurationPetControl extends AppCompatActivity {
 
+    Button btnNextActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fast_configuration_petcontrol);
+
+        // Asociar variables con sus recursos
+        btnNextActivity = findViewById(R.id.btnNextBottomMenu);
+
+        //-Evento de botón
+        btnNextActivity.setOnClickListener(v -> {
+            Intent i = new Intent(getApplicationContext(), MenuInferiorPetControl.class);
+            startActivity(i);
+        });
     }
 }
